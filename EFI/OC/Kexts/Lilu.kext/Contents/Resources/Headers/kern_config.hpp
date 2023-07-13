3819a7b73d2cefca49bad3c68ec1f883a545b532
GrieferPig <grieferpig@163.com>
2023-07-13 13:52:30 +0800

    Initial

---
+++ EFI/OC/Kexts/Lilu.kext/Contents/Resources/Headers/kern_config.hpp
@@ -0,0 +1,5 @@
+dition, Acer may also share such aggregate information about our visitors with advertisers, business partners, sponsors and other third parties, for example, to inform them of the percentage of male/female visitors or 
+\hich\af40\dbch\af14\loch\f40 t\hich\af40\dbch\af14\loch\f40 he percentage of visitors within a particular age range.
+\par 
+\par \hich\af40\dbch\af14\loch\f40 Security Measures:
+\par \hich\af40\dbch\af14\loch\f40 We protect the personal information which you share with us. At Acer, access to your personal information is limited to people who need it to do their work. It is our policy to ma\hich\af40\dbch\af1
